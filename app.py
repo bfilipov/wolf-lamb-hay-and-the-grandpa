52b701e94f3665ea4d845d8f5085e7a79707bfb3
Bogomil Filipov <filipov.bogomil@gmail.com>
2018-12-28 17:41:23 +0200

    Update app.py
    add name == main

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 import os
 from game.Game import Game
 
+if __name__ == "__main__":
+    g = Game()
+    while True:
+        os.system('cls')
+        g.print_state()
+        g.check_state()
+        if not g.game_over:
+            help_str = ""
+            for element in [g.first_brqg, g.second_brqg][g.current_brqg]:
+                help_str += "{} - {}\n".format(element.id, element.name)
 
-g = Game()
-while True:
-    os.system('cls')
-    g.print_state()
-    g.check_state()
-    if not g.game_over:
-        help_str = ""
-        for element in [g.first_brqg, g.second_brqg][g.current_brqg]:
-            help_str += "{} - {}\n".format(element.id, element.name)
-
-        print("В момента сме на бряг номер {}. \n Можем да преместим:\n{}".
-              format(g.current_brqg+1, help_str)
-              )
-        while True:
-            user_input = input()
-            if user_input in ["0", "1", "2", "3"]:
-                break
-            else:
-                print('Моля проверете въведената стойност')
-        g.move(user_input)
-    else:
-        break
+            print("В момента сме на бряг номер {}. \n Можем да преместим:\n{}".
+                  format(g.current_brqg+1, help_str)
+                  )
+            while True:
+                user_input = input()
+                if user_input in ["0", "1", "2", "3"]:
+                    break
+                else:
+                    print('Моля проверете въведената стойност')
+            g.move(user_input)
+        else:
+            break
